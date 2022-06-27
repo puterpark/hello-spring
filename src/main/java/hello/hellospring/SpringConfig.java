50cedb2e34ff61474138126b5b3568b009d14306
Puter <puterpark@me.com>
2022-06-27 20:20:34 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -24,30 +24,37 @@ public class SpringConfig {
 //		this.dataSource = dataSource;
 //	}
 
-	private EntityManager em;
+//	private EntityManager em;
+//
+//	@Autowired
+//	public SpringConfig(EntityManager em) {
+//		this.em = em;
+//	}
 
-	@Autowired
-	public SpringConfig(EntityManager em) {
-		this.em = em;
+	private final MemberRepository memberRepository;
+
+	public SpringConfig(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
 	}
 
 	/* 자바 코드로 직접 스프링 빈 등록하기 (서비스와 리포지토리를 등록) */
 
 	@Bean
 	public MemberService memberService() {
-		return new MemberService(memberRepository());
+//		return new MemberService(memberRepository());
+		return new MemberService(memberRepository);
 	}
 
-	@Bean
-	public MemberRepository memberRepository() {
-//		return new MemoryMemberRepository();
-		/**
-		 * 해당 부분만 바꿔서 H2 DB와 연결되도록 할 수 있다.
-		 * 기존 코드를 전혀 손대지 않고, 설정만으로 구현 클래스를 변경할 수 있다.
-		 */
-//		return new JdbcMemberRepository(dataSource);
-//		return new JdbcTemplateMemberRepository(dataSource);
-		return new JpaMemberRepository(em);
-	}
+//	@Bean
+//	public MemberRepository memberRepository() {
+////		return new MemoryMemberRepository();
+//		/**
+//		 * 해당 부분만 바꿔서 H2 DB와 연결되도록 할 수 있다.
+//		 * 기존 코드를 전혀 손대지 않고, 설정만으로 구현 클래스를 변경할 수 있다.
+//		 */
+////		return new JdbcMemberRepository(dataSource);
+////		return new JdbcTemplateMemberRepository(dataSource);
+//		return new JpaMemberRepository(em);
+//	}
 
 }
